a1d2e60877f772847241cc5b2ada1cfbd48721d0
Luis Henrique <iluishenriquebr@gmail.com>
2022-01-16 15:41:59 -0300

    Adição da classe MinhaDbContext, que herda a classe DbContext, que vai ser conectado com o SQL. Os DbSet vai representar a tabelas de "Cliente" e "Vendedor".

---
+++ EntityFramework/MinhaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    public class MinhaDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\mssqllocaldb;database=TesteEF;Trusted_Connection=true");
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Vendedor> Vendedores { get; set; }
+
+    }
+}
