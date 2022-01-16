5b28d4055c1e37d77b08dc0abea3e737b6396d43
Luis Henrique <iluishenriquebr@gmail.com>
2022-01-16 19:45:47 -0300

    Classe principal, Criando Clientes e Vendedores e adicionando no banco de dados.

--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -6,7 +6,19 @@ namespace EntityFramework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var baseDeDados = new MinhaDbContext())
+            {
+                var vendedor = new Vendedor();
+                var cliente = new Cliente();
+
+                baseDeDados.Clientes.Add(new Cliente { Nome = "Luis", Endereco = "Rua do Cajú" });
+                baseDeDados.Clientes.Add(new Cliente { Nome = "Lorena", Endereco = "Rua do Cajú" });
+
+                baseDeDados.Vendedores.Add(new Vendedor { Nome = "Henrique", MetadeVenda = 15 });
+                baseDeDados.Vendedores.Add(new Vendedor { Nome = "Eduardo", MetadeVenda = 28 });
+
+                baseDeDados.SaveChanges();
+            }
         }
     }
 }
